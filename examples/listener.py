34f8d326a8bc1425f303110e8aa808adf5d1c41c
glebovitz <gregg@lebovitz.net>
2026-01-06 19:49:23 -0500

    test

--- examples/listener.py
+++ examples/listener.py
@@ -143,9 +143,9 @@ async def main():
     radio.attach_loop(loop)
     radio.start_recv_loop()
 
-    ok = radio.request(RX_CONTINUOUS)
-    if not ok:
-        raise RuntimeError("Failed to enter RX_CONTINUOUS mode.")
+    # ok = radio.request(RX_CONTINUOUS)
+    # if not ok:
+    #     raise RuntimeError("Failed to enter RX_CONTINUOUS mode.")
     
     print(f"Radio status is {hex(radio.get_mode_and_control())}")
     # print(f"sync word is {hex(radio.get_sync_word())}")
